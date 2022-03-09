5df8a9eeecfcb83201d94bd133756152b43bde1e
arjunvishnu2000 <arjunts@ARJUNs-MBP.domain.name>
2022-03-09 12:01:05 +0530

    added preconfig

---
+++ my2.sh
@@ -0,0 +1,4 @@
+USERNAME='skskslsssss'
+PASSWORD='jdjdjdksslslslslslsllsls' #pragma: allowlist secret
+apikey="A1234567888665KKRFKDLLDLDDJDNNDMDDM" #pragma: allowlist secret
+echo "username: $PASSWORD"
